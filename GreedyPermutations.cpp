15e36eaf8ca7771f69acf727bec96a515dd6a296
tmease5580 <tmease5580@gmail.com>
2020-02-17 17:53:34 -0500

    Tested and working greedy perm

--- GreedyPermutations.cpp
+++ GreedyPermutations.cpp
@@ -4,45 +4,64 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <emscripten/bind.h>
 using namespace emscripten;
+
+
 
 
 std::vector<int> getGreedyPerm(std::vector<std::vector<float> > points, int NPerm)
 {
 	std::vector<int> indices;
+	//Creates vector of booleans whose values are true if the index has been added to indices or false if it hasn't been added yet
+	//Defaulted to all false values
+	std::vector<bool> visited(points.size(), false);
+
 	indices.push_back(0); //Puts on the first index so our list isn't empty
-    float INFINITY = std::numeric_limits<float>::infinity();
+	visited[0] = true; //Sets 0 to true since we have "visited" it
+
+	float INFINITY = std::numeric_limits<float>::infinity();
+
 	while (indices.size() < NPerm) {
 		int K = indices.size();
 		float minDistance = INFINITY;//Set to infinity so our first distance will be less than minDistance
 		std::vector<float> minDistances;//List of mindistances from each column
-		for (int i = 0; i < points.size();i++) {
-			for (int j = 0; j < K;j++) {
-				//Grabs current points
-				std::vector<float> point1 = points[i];
-				std::vector<float> point2 = points[indices[j]];
-				float distance = 0.0;
-				for (int m = 0; m < point1.size(); m++) {
-					distance += (point1[m] - point2[m])*(point1[m] - point2[m]);
-				}
-				//Compares current distance to minDistance and replaces if smaller
-				if (distance < minDistance) {
-					minDistance = distance;
+		int minIndex;
+		std::vector<int> minIndices;//Stores the indices of the point with the min distance associated with the equivalent place in the minDistances
+		for (int i = 0; i < K; i++) {
+
+			for (int j = 0; j < points.size(); j++) {
+				//If points[j] has not been visited, calculate the distance
+				//Otherwise we skip over j and move to the next point
+				if (!visited[j]) {
+					std::vector<float> point1 = points[j];
+					std::vector<float> point2 = points[indices[i]];
+					float distance = 0.0;
+					for (int m = 0; m < point1.size(); m++) {
+						distance += (point1[m] - point2[m]) * (point1[m] - point2[m]);
+					}
+					//Compares current distance to minDistance and replaces if smaller, if equal to 0, we compared the same points and we don't want to repeat
+					if (distance < minDistance) {
+						minDistance = distance;
+						minIndex = j;
+					}
 				}
 			}
+			minIndices.push_back(minIndex);
 			minDistances.push_back(minDistance);//adds the mindistance for this set to the list
 		}
 		//finds the max value of our minDistances
 		float maxMinDistance = *std::max_element(minDistances.begin(), minDistances.end());
 		//Finds the index of the max value
 		std::vector<float>::iterator it = find(minDistances.begin(), minDistances.end(), maxMinDistance);
-		int index = std::distance(minDistances.begin(), it);
+		int index = it - minDistances.begin();
+		index = minIndices[index];
 		//Add the index of the max of this list to the indices list
 		indices.push_back(index);
-	}
-	for (int i = 0; i < indices.size(); i++) {
-		std::cout << indices[i];
+		//Set the index of our point to true since we have visited it
+		visited[index] = true;
 	}
 	return indices;
 }
