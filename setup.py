17cd2b1cb164f6c298cce33234e51bc043d94706
Michael Catanzaro <catanzaromj@pm.me>
2024-09-09 13:54:28 -0400

    Convert to pyproject.toml spec (#18)
    * Convert setup.py spec to pyproject.toml spec
    
    * Remove requirements.txt
    
    * Update workflows to use optional installs
    
    * Add pytestcov to optional testing deps
    
    * Fix two dep typos and add build dep,
    
    * Optional dep is called 'testing' not 'tests'
    
    * Fix windows tests
    
    * Add doc requirements
    
    * Remove changelog from pyproject.toml

--- setup.py
+++ setup.py
@@ -1,55 +1,18 @@
 from setuptools import setup
-
-## Get version information from _version.py
 import re
 
-VERSIONFILE = "dreimac/_version.py"
-verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    verstr = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-# Use README.md as the package long description
-with open("README.md") as f:
-    long_description = f.read()
-
-
-# get requirements
-def requirements():
-    with open("requirements.txt") as f:
-        return [line.strip() for line in f if line.strip()]
+def get_version():
+    VERSIONFILE = "dreimac/_version.py"
+    verstrline = open(VERSIONFILE, "rt").read()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 
 setup(
-    name="dreimac",
-    version=verstr,
-    description="DREiMac: Dimensionality reduction with Eilenberg-MacLane coordinates",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Jose A. Perea, Luis Scoccola, Chris Tralie",
-    author_email="ctralie@alumni.princeton.edu",
-    license="Apache2",
-    packages=["dreimac"],
-    install_requires=requirements(),
-    extras_require={
-        "testing": ["pytest"],
-        "docs": ["sphinx", "sphinx_rtd_theme", "numpydoc", "ipykernel", "nbsphinx"],
-    },
-    python_requires=">=3.8,<3.12",
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Education",
-        "Intended Audience :: Financial and Insurance Industry",
-        "Intended Audience :: Healthcare Industry",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-    keywords="topological data analysis, dimensionality reduction",
+    version=get_version(),
 )
